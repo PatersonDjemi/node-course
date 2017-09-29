6ea946e22155c1dd9b780a07c97ab93ffd4ee0c3
Paterson <djemipaterson@yahoo.fr>
2017-09-29 19:32:52 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+
+
+
+app.set('view engine', 'hbs');
+
+// use permet d utliser les middelwares
+app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+
+   var now = new Date().toString();
+   var log = req.method + ' ' + req.url + ' ' + ' ' + now;
+
+   console.log(log);
+
+   fs.appendFile('server.log', log + '\n');
+
+      //req.url ns renvoit l url de la page sur laquelle on se trouve,
+      // req.mehtod ns renvoit le type de methode http utilisé (GET, POST, ...)
+
+   next();
+});
+
+
+app.use(function (req, res, next) {
+    res.render('basic.hbs');
+})
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', function () {
+   return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', function (text) {
+    return text.toUpperCase();
+});
+
+app.get('/',  function (req, res) {
+   //res.send('<h1>hello express</h1>');
+    /*
+    res.send({
+        name: 'Paterson',
+        todo: 'build a startup'
+    })
+    */
+    res.render('home.hbs', {
+       pageTitle: ' The Home Page',
+        welcomeMessage: 'welcome to the home page NoFlemmeZone',
+        //currentYear: new Date().getHours()
+    })
+});
+
+app.get('/about', function (req, res) {
+   // render permet de render un template
+    res.render('about.hbs', {
+       pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', function (req, res) {
+    res.send({
+       errorMessage: "unable to open that page"
+    });
+});
+
+
+
+
+app.listen(3000, function () {
+    console.log('server is up in port 3000')
+});
